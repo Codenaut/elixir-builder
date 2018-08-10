44abe73cfa9d447418d3d7ac8181fe6099f50084
Jesper André Lyngesen Pedersen <jalpedersen@gmail.com>
2018-08-10 13:15:02 +0200

    add git rev to release version

--- build-release.sh
+++ build-release.sh
@@ -11,7 +11,20 @@ builddir=$source/amd64/_build
 depsdir=$source/amd64/deps
 reldir=$source/amd64/rel
 
-mix="docker run --rm -v $libs:/libs -v $source:/build -v $builddir:/build/_build -v $depsdir:/build/deps -v $reldir:/build/rel codenaut/exrm-builder mix "
+GIT_REV=`git rev-parse --short HEAD`
+# Replace the version in rel/config.exs with something like this:
+#    set version: "0.1.0-" <> System.get_env("GIT_REV")
+
+if [ "x" != "x`git diff-files`"  ]; then
+    echo "You have uncommitted files. Please commit them before building release"
+    exit 1
+fi
+if [ "x" != "x`git diff-index origin/master`"  ]; then
+    echo "You have unpushed local commits. Please push them before building release"
+    exit 1
+fi
+
+mix="docker run --rm -e GIT_REV=$GIT_REV -v $libs:/libs -v $source:/build -v $builddir:/build/_build -v $depsdir:/build/deps -v $reldir:/build/rel codenaut/exrm-builder mix "
 
 $mix clean
 $mix deps.get
